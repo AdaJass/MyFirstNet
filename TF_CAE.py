cdb39c44391341886a12f22830d06ddf406cceb5
Jass Ada <Jass Ada>
2018-01-02 19:19:49 +0800

    Dkjg

--- TF_CAE.py
+++ TF_CAE.py
@@ -108,11 +108,11 @@ with tf.Session() as sess:
     sess.run(init)  
     for epoch in range(train_epochs):  
         for batch_index in range(total_batch):  
-            batch_x, _ = mnist.train.next_batch(batch_size)  
+            batch_x, _ = mnist.train.next_batch(batch_size) 
+            print(batch_x,'\n\n') 
             noise_x = batch_x + noise_factor * np.random.randn(*batch_x.shape)  
             noise_x = np.clip(noise_x, 0., 1.)  #make the noise limited in [0, 1]
-            _, train_loss = sess.run([optimizer, loss], feed_dict={input_x: noise_x, input_raw: batch_x})  
-            print(noise_x, '\n', batch_x)
+            resulll, train_loss = sess.run([output, loss], feed_dict={input_x: noise_x, input_raw: batch_x})  
             print('epoch: %04d\tbatch: %04d\ttrain loss: %.9f' % (epoch + 1, batch_index + 1, train_loss))  
   
     ## 训练结束后，用测试集测试，并保存加噪图像、去噪图像  
